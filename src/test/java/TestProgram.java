637923dc0ee19eb5827346b410027d69d87d68e7
piopao <ekikn12@freeuni.edu.ge>
2016-05-08 00:31:06 +0400

    JUnit test added

---
+++ src/test/java/TestProgram.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Created by elene on 5/8/16.
+ */
+
+public class TestProgram {
+
+    @Test
+    public void AssertionTest() {
+        assert(3!=5);
+    }
+
+
+}
