80ad99a9357ff0cf5805eed126fcce0ad2cf2503
piopao <ekikn12@freeuni.edu.ge>
2016-05-08 01:00:55 +0400

    Testable class added

---
+++ src/main/java/Mecxre.java
@@ -0,0 +1,11 @@
+/**
+ * Created by elene on 5/8/16.
+ */
+public class Mecxre {
+    public String haveAGoodNight(boolean isAlive){
+        if(isAlive) return "sawoli";
+        else return "sarkofagi";
+    }
+
+
+}
