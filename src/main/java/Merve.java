d18a17a0f39e57f254c2cc878b1549bf81fd0124
piopao <ekikn12@freeuni.edu.ge>
2016-05-08 00:13:34 +0400

    Program added

---
+++ src/main/java/Merve.java
@@ -0,0 +1,11 @@
+/**
+ * Created by elene on 5/8/16.
+ */
+public class Merve {
+    public static void main(String[] args){
+        System.out.print("Feel no shame about shape \n" +
+                "Weather changes their phrase \n" +
+                "Even mother will show you another way ");
+
+    }
+}
